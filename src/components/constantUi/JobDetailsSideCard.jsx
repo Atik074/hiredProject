a1dedc98a43c6190fe8faee26be0c740fa9f8457
Atik074 <atiksmats#@gmail.com>
2025-01-27 23:46:55 +0600

    job details page design with sideslide

---
+++ src/components/constantUi/JobDetailsSideCard.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const JobDetailsSideCard = () => {
+    return (
+        <div className="w-[30%] relative">
+            <img className="mb-8 w-full h-[260px]"  src="https://shorturl.at/cUQVZ" alt="" />
+
+            <div className="absolute top-20">
+               <h3 className="text-[27px] mb-4  text-center text-white">Your logic building is your foundation</h3>
+               <Link to="" className="bg-red-600 px-14 py-2 rounded text-xl text-white font-medium text-center mx-28 hover:text-white ">Learn</Link>
+            </div>
+
+
+            <img className="mb-8 w-full h-[260px] " src="https://shorturl.at/ma4hP" alt="" />
+
+            <div className="absolute top-[370px]">
+               <h3 className="text-[27px] mb-4  text-center text-white">To get Job fundamental is recommended</h3>
+               <Link to="" className="bg-red-600 px-14 py-2 rounded text-xl text-white font-medium text-center mx-28 hover:text-white ">Achieve</Link>
+               
+            </div>
+
+
+
+
+
+
+            
+
+
+            <img className="mb-8 w-full h-[260px]" src="https://shorturl.at/D26Jt" alt="" />
+
+            <div className="absolute top-[665px]">
+               <h3 className="text-[27px] mb-4  text-center text-white"> Problem solving skill gives    oppurtunity</h3>
+               <Link to="" className="bg-red-600 px-14 py-2 rounded text-xl text-white font-medium text-center mx-28 hover:text-white ">Test</Link>
+            </div>
+            <img className="mb-8 w-full h-[260px]" src="https://shorturl.at/aIlIJ" alt="" />
+
+            <div className="absolute bottom-[450px]">
+               <h3 className="text-[27px] mb-4 text-center text-white">Communication skill  opens a new window</h3>
+               <Link to="" className="bg-red-600 px-14 py-2 rounded text-xl text-white font-medium text-center mx-28 hover:text-white ">Learn</Link>
+            </div>
+            <img className="mb-8 w-full h-[260px]" src="https://shorturl.at/uFa8Y" alt="" />
+
+            <div className="absolute bottom-[155px] ">
+               <h3 className="text-[27px] mb-4 text-center  text-white">Your leadship mind helps to grow</h3>
+               <Link to="" className="bg-red-600 px-14 py-2 rounded text-xl text-white font-medium text-center mx-28 hover:text-white ">Gain</Link>
+            </div>
+              
+          </div>
+    );
+};
+
+export default JobDetailsSideCard;
--- src/pages/featureJobs/FeatureJobDetails.jsx
+++ src/pages/featureJobs/FeatureJobDetails.jsx
@@ -6,25 +6,28 @@ import { TbFileDescription } from "react-icons/tb";
 import { IoMdArrowForward } from "react-icons/io";
 import { IoBagHandleOutline } from "react-icons/io5";
 import { MdOutlineOtherHouses } from "react-icons/md";
-
-
-
-
-
-
-
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+import JobDetailsSideCard from "@/components/constantUi/JobDetailsSideCard";
 
 const FeatureJobDetails = () => {
   const { job } = useContext(JobDataContext);
-  const { image, post_name, job_type, salary_range, location, category,description,requirements } = job;
-
- 
+  const {
+    image,
+    post_name,
+    job_type,
+    salary_range,
+    location,
+    description,
+    requirements,
+    title,
+    vacancy,
+    category,
+  } = job;
 
   return (
-    <div className="jobDetails">
-      <div className="w-full h-[400px] opacity-[.7] bg-cover  bg-[url('https://shorturl.at/VZCnr')]">
-     
-      </div>
+    <div>
+      <div className="w-full h-[400px] opacity-[.7] bg-cover  bg-[url('https://shorturl.at/VZCnr')] mb-20"></div>
       <div className=" mx-12 flex items-center gap-x-8 absolute top-[20%]">
         <img className="w-40 h-40 rounded-full" src={image} alt="companyLogo" />
 
@@ -32,7 +35,9 @@ const FeatureJobDetails = () => {
           <span className="bg-[#e0f5d7] text-[#449626] text-[16px] px-2 rounded">
             {job_type}
           </span>
-          <h2 className="mt-2 mb-3 text-3xl font-semibold text-white">{post_name}</h2>
+          <h2 className="mt-2 mb-3 text-3xl font-semibold text-white">
+            {post_name}
+          </h2>
           <div className="flex gap-x-4">
             <p className="flex item-center justify-center  shadow-sm p-1 rounded text-white    border border-[#c6bcbc7e]  font-medium">
               <CiLocationOn className="text-[20px] mx-2" />
@@ -46,42 +51,83 @@ const FeatureJobDetails = () => {
         </div>
       </div>
 
-      <div className="container my-[120px ">
-       <img className="w-[70%]" src="https://shorturl.at/PTyMD" alt="" />
-         <div className="bg-gray-700 flex items-center gap-x-2 p-3 my-6 rounded ">
-       <TbFileDescription className="  border border-[#898686] bg-white rounded-full w-9 h-9 p-[6px]" /> 
-       <h2 className="text-[21px] font-medium  text-white">Job Descrition</h2>
-         </div>
-         <p className="text-justify text-[19px]">{description}</p>
-
-
-         <div className="bg-gray-700 flex items-center gap-x-2 p-3 my-6 rounded ">
-       <IoBagHandleOutline className="border border-[#898686] bg-white rounded-full w-9 h-9 p-[6px]" /> 
-       <h2 className="text-[21px] font-medium  text-white"> Required Skills and Abilities</h2>
-         </div>
-         <p className="text-justify text-[19px]">{requirements.map(item =><li key={item}
-         className="list-none flex items-center gap-x-2 mb-2"
-         >
-          <IoMdArrowForward className="text-[19px] text-amber-600 font-medium"/>{item }
-          </li>)}</p>
-
+      <div className="container  flex justify-between">
+        <div className="w-[67%]">
+          <img className="w-[100%]" src="https://shorturl.at/PTyMD" alt="" />
+          <div className="bg-gray-700 flex items-center gap-x-2 p-3 my-6 rounded ">
+            <TbFileDescription className="  border border-[#898686] bg-white rounded-full w-9 h-9 p-[6px]" />
+            <h2 className="text-[21px] font-medium  text-white">
+              Job Descrition
+            </h2>
+          </div>
+          <p className="text-justify text-[19px]">{description}</p>
 
           <div className="bg-gray-700 flex items-center gap-x-2 p-3 my-6 rounded ">
-       <MdOutlineOtherHouses className="  border border-[#898686] bg-white rounded-full w-9 h-9 p-[6px]" /> 
-       <h2 className="text-[21px] font-medium  text-white">Others Information</h2>
-         </div>
-         <li className="text-justify text-[20px] font-normal">salary_range : {salary_range}</li>
-         <li className="text-justify text-[20px] font-normal">category : {salary_range}</li>
-         <li className="text-justify text-[20px] font-normal">vacancy : {salary_range}</li>
-         <li className="text-justify text-[19px] font-normal">title : {salary_range}</li>
-
-
-
-        
-
-          <div className="w-[30%]">
-              
+            <IoBagHandleOutline className="border border-[#898686] bg-white rounded-full w-9 h-9 p-[6px]" />
+            <h2 className="text-[21px] font-medium  text-white">
+              {" "}
+              Required Skills and Abilities
+            </h2>
           </div>
+          <p className="text-justify text-[19px]">
+            {requirements.map((item) => (
+              <li
+                key={item}
+                className="list-none flex items-center gap-x-2 mb-2"
+              >
+                <IoMdArrowForward className="text-[19px] text-amber-600 font-medium" />
+                {item}
+              </li>
+            ))}
+          </p>
+
+          <div className="bg-gray-700 flex items-center gap-x-2 p-3 my-6 rounded ">
+            <MdOutlineOtherHouses className="  border border-[#898686] bg-white rounded-full w-9 h-9 p-[6px]" />
+            <h2 className="text-[21px] font-medium  text-white">
+              Others Information
+            </h2>
+          </div>
+          <h2 className="text-[22px]   mb-3 underline underline-offset-8">
+            Primary Responsivilty :
+          </h2>
+          <p className="text-[19px] mx-4 text-justify mb-4">
+            Depending on the role, you may be responsible for managing or
+            coordinating specific projects. This could include setting
+            deadlines, allocating resources, tracking progress, and
+            communicating with team members or clients.Working effectively with
+            others within a team is often essential. If you're in a leadership
+            role, you might have additional responsibilities, such as delegating
+            tasks, providing feedback, and guiding team members to meet
+            objectives
+          </p>
+
+          <div className="flex items-center gap-x-3  text-[20px] font-normal mb-2">
+            <IoMdArrowForward className="text-[19px] text-amber-600 font-medium" />
+            <p className="-mt-2">Company : {title}</p>
+          </div>
+          <div className="flex items-center gap-x-3  text-[20px] font-normal mb-2">
+            <IoMdArrowForward className="text-[19px] text-amber-600 font-medium" />
+            <p className="-mt-2">Category : {category}</p>
+          </div>
+          <div className="flex items-center gap-x-3  text-[20px] font-normal mb-2">
+            <IoMdArrowForward className="text-[19px] text-amber-600 font-medium" />
+            <p className="-mt-2">Vacancy : {vacancy}</p>
+          </div>
+          <div className="flex items-center gap-x-3   text-[20px] font-normal mb-2">
+            <IoMdArrowForward className="text-[19px] text-amber-600 font-medium" />
+            <p className="-mt-2">Yearly Range : {salary_range}</p>
+          </div>
+
+          <Button
+            className="text-white w-1/4 text-xl"
+            variant="destructive"
+            size="lg"
+          >
+            Apply for Job
+          </Button>
+        </div>
+
+        <JobDetailsSideCard />
       </div>
     </div>
   );
