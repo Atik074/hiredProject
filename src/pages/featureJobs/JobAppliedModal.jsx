cfc26a0d3d0128062c50382358f48778471dcd87
Atik074 <atiksmats#@gmail.com>
2025-01-29 10:44:48 +0600

    error show on modal try to solve it

--- src/pages/featureJobs/JobAppliedModal.jsx
+++ src/pages/featureJobs/JobAppliedModal.jsx
@@ -2,10 +2,17 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
+import { useForm } from 'react-hook-form';
 import { RxCross2 } from "react-icons/rx";
 
 
 const JobAppliedModal = ({setShowModal}) => {
+  const { register, handleSubmit } = useForm()
+
+  //handle applied job data
+  const onSubmit = (data) =>{
+    console.log( "modal job data from modal" , data)
+  }
  
 
   
@@ -13,41 +20,41 @@ const JobAppliedModal = ({setShowModal}) => {
         <div className=" fixed inset-0  mx-auto   z-50 bg-black/60 backdrop-blur-sm flex justify-center items-center">
      
 
-         <form className=" bg-zinc-900 px-20 py-10 rounded-md shadow-2xl">
+         <form onSubmit={handleSubmit(onSubmit)} className=" bg-zinc-900 px-20 py-10 rounded-md shadow-2xl">
           
       
          <div className="grid w-full max-w-sm items-center gap-1.5 mb-3">
         <Label htmlFor="name">Your Name</Label>
-        <Input type="name" id="name" placeholder="name" />
+        <Input type="name" id="name" placeholder="name" {...register("name", { required: true, maxLength: 20 })} />
         </div>
 
         <div className="grid w-full max-w-sm items-center gap-1.5 ">
         <Label htmlFor="email">Your Email</Label>
-        <Input type="email" id="email" placeholder="Email" />
+        <Input type="email" id="email" placeholder="Email" {...register("email", { required: true})}/>
         <div className="grid w-full gap-2">
 
 
         <div className="grid w-full max-w-sm items-center gap-1.5 mb-3 mt-3">
         <Label htmlFor="number">Your Phone Number</Label>
-        <Input type="number" id="number" placeholder="phone number" />
+        <Input type="number" id="number" placeholder="phone number" {...register("number", { required: true})} />
         </div>
 
         
 
         <div className="grid w-full max-w-sm items-center gap-1.5 mb-3">
        <Label htmlFor="picture">Your resume</Label>
-       <Input id="picture" type="file" />
+       <Input id="picture" type="file" {...register("file", { required: true})} />
        </div>
 
 
     
       <div className="grid w-full gap-1.5 mb-3">
       <Label htmlFor="message">Say, something</Label>
-      <Textarea placeholder="Shortly ,type your extra creativity." id="message" />
+      <Textarea type="message" placeholder="Shortly ,type your extra creativity.( optional!)" id="message"   />
     </div>
     
       <div className="grid w-full gap-1.5 mb-3">
-        <Button className="text-xl" variant="default" size="lg">submit</Button>
+        <button className="text-xl py-2 bg-zinc-900 border border-white text-white" >submit</button>
     </div>
       
      </div>
