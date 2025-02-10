6aa0aec9e0a3d17c0a1a3e6e9feeff4e419ba35c
atik074 <atiksmats@gmail.com>
2025-02-10 14:45:33 +0530

    sidebar and main part divided for all jobs showing

--- src/pages/allJobs/AllJobs.jsx
+++ src/pages/allJobs/AllJobs.jsx
@@ -1,10 +1,36 @@
+import { JobDataContext } from "@/context/AuthContext";
+import useJobData from "@/hooks/useData";
+import { useContext } from "react";
 
 const AllJobs = () => {
+  const  {jobs} = useJobData()
+
+  console.log("from alljobs" , jobs)
+
     return (
-        <div className="container grid grid-cols-[250px,1fr] mt-12  h-screen gap-20">
-              <div className="sidebar border border-red-600">
-                side bar coming..
-                    <h2 className="text-xl mb-3 border-b-2">Industry</h2>
+        <div className="container grid grid-cols-[250px,1fr] mt-12  h-screen gap-12 p-10">
+              <div className="sidebar ">
+             
+                    <h2 className="text-xl mb-3 border-b-2">Search by Categories</h2>
+                  <div className="flex items-center  gap-2 ">
+                    <input className="w-4 h-4 border-2 border-sky-700"  type="checkbox" name="checkbox" id="checkbox" />
+                    <label className="text-[19px]">Programming</label>
+                  </div>
+                  <div className="flex items-center  gap-2">
+                    <input className="w-4 h-4 border-2 border-sky-700"  type="checkbox" name="checkbox" id="checkbox" />
+                    <label className="text-[19px]">Marketing</label>
+                  </div>
+                  <div className="flex items-center  gap-2">
+                    <input className="w-4 h-4 border-2 border-sky-700"  type="checkbox" name="checkbox" id="checkbox" />
+                    <label className="text-[19px]">Management</label>
+                  </div>
+                  <div className="flex items-center  gap-2">
+                    <input className="w-4 h-4 border-2 border-sky-700"  type="checkbox" name="checkbox" id="checkbox" />
+                    <label className="text-[19px]">Networking</label>
+                  </div>
+
+                  {/* ////////////// */}
+                  <h2 className="text-xl mb-3 border-b-2 mt-6">Job type</h2>
                   <div className="flex items-center  gap-2">
                     <input className="w-4 h-4 border-2 border-sky-700"  type="checkbox" name="checkbox" id="checkbox" />
                     <label className="text-[19px]">Software</label>
@@ -21,11 +47,24 @@ const AllJobs = () => {
                     <input className="w-4 h-4 border-2 border-sky-700"  type="checkbox" name="checkbox" id="checkbox" />
                     <label className="text-[19px]">Software</label>
                   </div>
+                  {/* /////// */}
                   
                 </div>
 
 
-              <div className="mainPart border border-green-600"></div>
+              <div className="mainPart border border-green-600">
+                  <div className="w-full h-12 border-2 border-red-800">
+                      
+
+                  </div>
+                  <h2 className="text-[23px] font-medium">Latest Jobs Available Here</h2>
+                  <p className="text-[18px] text-[#706d6d]">Get your Dream job from top companies by Hirrd</p>
+
+                  {
+
+                  }
+
+              </div>
         </div>
     );
 };
